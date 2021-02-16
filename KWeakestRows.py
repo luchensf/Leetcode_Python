bb144e2cd90b3841531b3e56abe308417752118c
lunoply <luchensf@gmail.com>
2021-02-15 22:40:25 -0800

    modify KWeakestRows.py

--- KWeakestRows.py
+++ KWeakestRows.py
@@ -1,14 +1,11 @@
-def kWeakestRows(matrix, k):
+def kWeakestRows(mat, k):
     counter = {}
     for i in range(len(mat)):
         counter[i] = sum(mat[i])
     s = sorted(counter, key=counter.get)[:k]
     return s
 
-
-
-
-kWeakestRows([[1,1,0,0,0],[1,1,1,1,0],[1,0,0,0,0], [1,1,0,0,0], [1,1,1,1,1]], 3)
+# kWeakestRows([[1,1,0,0,0],[1,1,1,1,0],[1,0,0,0,0], [1,1,0,0,0], [1,1,1,1,1]], 3)
 
 
 
